52b549ee11d8f739b7ddac298137e076bada1a4a
Dan Krishnan <dhani.krishnan@theverygroup.com>
2023-12-01 16:04:36 +0000

    refactor: day01 into unreadable code

--- TypeScript/day01/part1.ts
+++ TypeScript/day01/part1.ts
@@ -2,15 +2,15 @@ import { getPuzzleInput } from "../utils";
 const input = getPuzzleInput("day01");
 
 // Had a nested for loop method for the answer, but refactored it to below.
+// Which is shit and unreadable an no one should ever actually write code like this.
 
 export default function part1(input: string) {
-  const lines = input.split("\n");
-
-  const numbers = lines
+  return input
+    .split("\n")
     .map((line) => line.split("").filter((item) => Number(item)))
-    .map((nums) => nums[0] + nums[nums.length - 1]);
-
-  return numbers.map(Number).reduce((prev, cur) => prev + cur);
+    .map((nums) => nums[0] + nums[nums.length - 1])
+    .map(Number)
+    .reduce((prev, cur) => prev + cur);
 }
 
 /*
@@ -18,6 +18,5 @@ Explanation:
 Turn each line into an array.
 Filter out all the letters so you just have numbers left
 Turn each array into the concat of the first and the last number (string)
-
 map and reduce the final array.
 */
