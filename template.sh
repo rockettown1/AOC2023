91ad7141308e3d89a140779be46edf976243d5dc
Dan Krishnan <dhani.krishnan@theverygroup.com>
2023-12-02 22:08:00 +0000

    chore: update template script

--- template.sh
+++ template.sh
@@ -10,11 +10,17 @@ done
 green='\033[0;32m'
 blue='\033[0;34m'
 
-cd "$language"
-mkdir -p day"$day"
-cp -r ./template/. day"$day"
-echo "${green}Created solution directory for day$day in $language"
 
-cd ../_inputs
-touch day"$day".txt
+cd $language
+mkdir -p day$day
+cp -r ./template/. day$day
+echo "${green}Created solution directory for day$day in $language"
+cd day$day
+mv \$day.spec.ts day$day.spec.ts
+sed -i '' "s/\$day/$day/" day$day.spec.ts
+sed -i '' "s/\$day/$day/" part1.ts
+sed -i '' "s/\$day/$day/" part2.ts
+
+cd ../../_inputs
+touch day$day.txt
 echo "${blue}Created txt file for input if it wasn't already there."
